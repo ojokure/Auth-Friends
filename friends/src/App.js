faca6bca171097e1489821e6ece78c4583879fbd
oladimeji-ojo <ojokuredim@gmail.com>
2019-10-15 12:32:14 +0100

    created a friends componen t

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, NavLink, withRouter, Redirect } from 'react-router-dom';
 import './App.css';
 import Login from "./Login";
+import Friends from "./Friends";
+
 
 function App() {
   return (
     <div className="App">
      <Login/>
+     <Friends/>
     </div>
   );
 }
